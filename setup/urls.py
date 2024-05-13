adb560a7b84ca1af3da0b8501fdde11c6513c8f2
phaleixo <phaleixo1@gmail.com>
2024-05-13 18:16:36 -0300

    Changes to be committed:

---
+++ setup/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import RedirectView
+
+from todos.views import (
+    TodoListView,
+    TodoCreateView,
+    TodoUpdateView,
+    TodoDeleteView,
+    TodoCompleteView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", TodoListView.as_view(), name="todo_list"),
+    path("create", TodoCreateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
+    path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
+    path("complete/<int:pk>", TodoCompleteView.as_view(), name="todo_complete"),
+    path('', RedirectView.as_view(url='/', permanent=True)),
+]
